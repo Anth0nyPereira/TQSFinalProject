e46b2c0c65c2655ed30776b56674c4129ca905ef
JoaoTS20 <joaots@ua.pt>
2021-05-30 01:45:24 +0100

    Add Nav Bar Navigation

---
+++ EasyDelivers/RiderApp/app/src/main/java/com/example/riderapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.riderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        BottomNavigationView navView = findViewById(R.id.bottom_navigatin_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment, R.id.encomendasFragment, R.id.profileFragment)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+}
