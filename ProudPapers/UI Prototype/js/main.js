3f0e1bf56f51ad6cce55947765eba361e84ebac9
Wei93442 <al15020@hotmail.com>
2021-05-31 19:58:45 +0100

    product page

--- ProudPapers/UI Prototype/js/main.js
+++ ProudPapers/UI Prototype/js/main.js
@@ -5,3 +5,41 @@ function showBtn() {
 function hiddenBtn() {
     $('.carousel-control').attr('hidden', 'hidden')
 }
+
+function inc(){
+   var curr =  $(".cart-plus-minus-box").val()
+   $(".cart-plus-minus-box").val(parseInt(curr) + 1)
+}
+
+function dec(){
+    var curr =  $(".cart-plus-minus-box").val()
+    $(".cart-plus-minus-box").val(parseInt(curr) - 1)
+}
+
+function descr(){
+    if($('.rev').hasClass('active')){
+        $('.rev').removeClass('active')
+    }
+
+    if(!$('.descr').hasClass('active')){
+        $('.descr').addClass('active')
+    }
+
+    $('#rev').attr('hidden', 'hidden');
+    $('#descr').removeAttr('hidden');
+}
+
+
+function reviews(){
+    if(!$('.rev').hasClass('active')){
+        $('.rev').addClass('active')
+    }
+
+    if($('.descr').hasClass('active')){
+        $('.descr').removeClass('active')
+    }
+
+
+    $('#descr').attr('hidden', 'hidden');
+    $('#rev').removeAttr('hidden');
+}
