e4feeda4e47ba30003ec955d9a38696dede089c8
Anth0nyPereira <anthonypereira@ua.pt>
2021-06-01 08:53:51 +0100

    Links to each rider dashboard, variables for statistical data created

--- EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/controller/EasyDeliversController.java
+++ EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/controller/EasyDeliversController.java
@@ -1,10 +1,14 @@
 package ua.deti.tqs.easydeliversadmin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DecimalFormat;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/")
@@ -46,7 +50,17 @@ public class EasyDeliversController {
     }
 
     @GetMapping("/employee")
-    public String singular(){
+    public String singular(@RequestParam(value="id") int id, Model model){
+        Random randomNumb = new Random();
+        int kms = randomNumb.nextInt(40);
+        int deliveries = randomNumb.nextInt(40);
+        int time = randomNumb.nextInt(40);
+        DecimalFormat df = new DecimalFormat("#.#");
+        double score = 5 * randomNumb.nextDouble();
+        model.addAttribute("kms", kms);
+        model.addAttribute("deliveries", deliveries);
+        model.addAttribute("time", time);
+        model.addAttribute("score", df.format(score));
         return "riderDashboard";
     }
 
