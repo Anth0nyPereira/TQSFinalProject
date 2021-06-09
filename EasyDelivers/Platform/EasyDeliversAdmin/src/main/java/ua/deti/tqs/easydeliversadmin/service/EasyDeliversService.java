5db8a352aecbd3bddad0c7ccb6276f5232f6db3f
JoaoTS20 <joaots@ua.pt>
2021-06-09 02:47:16 +0100

    Service Login/Sign Up Rider Implemented

--- EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/service/EasyDeliversService.java
+++ EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/service/EasyDeliversService.java
@@ -15,15 +15,20 @@ public class EasyDeliversService {
     RiderRepository riderRepository;
 
     public boolean authenticateRider(String email, String password) {
-        return true;
+        // Hash Password to consider
+        Rider x = riderRepository.findRiderByEmail(email);
+        if(x!=null && x.getPassword().equals(password)) {
+            return true;
+        }
+        return false;
     }
 
     public Rider getRider(String email) {
-        return null;
+        return riderRepository.findRiderByEmail(email);
     }
 
     public Rider createRider(String firstname, String lastname, String email, String password, String telephone, String transportation) {
-        return null;
+        return riderRepository.save(new Rider(firstname,lastname,email,password,telephone,transportation));
     }
 
     public String createDelivery(int store, String client_telephone, String start, String destination) {
