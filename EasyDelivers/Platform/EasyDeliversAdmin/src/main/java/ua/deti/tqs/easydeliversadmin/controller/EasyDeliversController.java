8c88cce651decae50f549a3383cd4108f3e4bd88
Anth0nyPereira <anthonypereira@ua.pt>
2021-06-13 11:37:01 +0100

    Updated controllers - made some redirects using ModelAndView

--- EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/controller/EasyDeliversController.java
+++ EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/controller/EasyDeliversController.java
@@ -3,10 +3,12 @@ package ua.deti.tqs.easydeliversadmin.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import ua.deti.tqs.easydeliversadmin.entities.Admin;
 import ua.deti.tqs.easydeliversadmin.service.EasyDeliversService;
 import ua.deti.tqs.easydeliversadmin.utils.PasswordEncryption;
@@ -25,20 +27,32 @@ public class EasyDeliversController {
     @Autowired
     EasyDeliversService service;
 
+    @GetMapping("/")
+    public ModelAndView loginOrDashboard(ModelMap model) {
+        if (session.equals("")) {
+            return new ModelAndView("redirect:/login", model);
+        } else {
+            return new ModelAndView("redirect:/dashboard", model);
+        }
+    }
+
     @GetMapping("/logout")
-    public String logout(){
+    public ModelAndView logout(ModelMap model){
         session = "";
-        return "login";
+        return new ModelAndView("redirect:/login", model);
     }
 
     @GetMapping("/login")
-    public String login(){
-        log.warning("LOGIN");
-        return "login";
+    public ModelAndView login(ModelMap model){
+        if (session.equals("")) {
+            return new ModelAndView("login", model);
+        } else {
+            return new ModelAndView("redirect:/dashboard", model);
+        }
     }
 
-    @PostMapping("/dashboard")
-    public String login(@RequestParam(value = "email", defaultValue = "ola")String email, @RequestParam(value = "password", defaultValue = "ola")String password) throws EasyDeliversService.AdminNotFoundException, Exception {
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam(value = "email", defaultValue = "hello")String email, @RequestParam(value = "password", defaultValue = "bye")String password, ModelMap model) throws EasyDeliversService.AdminNotFoundException, Exception {
         log.warning("LOGS");
         Admin admin = service.getAdminByEmail(email);
         log.info("Email: " + email);
@@ -47,19 +61,19 @@ public class EasyDeliversController {
         PasswordEncryption encryptor = new PasswordEncryption();
         if (admin.getPassword().equals(encryptor.encrypt(password))) {
             session = email;
-            return "dashboard";
+            return new ModelAndView("redirect:/dashboard", model);
         } else{
-            return "login";
+            return new ModelAndView("login", model);
         }
     }
 
     @GetMapping("/dashboard")
-    public String dash(){
+    public ModelAndView dash(ModelMap model){
         log.info(session);
         if(!session.equals(""))
-            return "dashboard";
+            return new ModelAndView("dashboard", model);
         else
-            return "login";
+            return new ModelAndView("redirect:/login", model);
     }
 
     @GetMapping("/account")
