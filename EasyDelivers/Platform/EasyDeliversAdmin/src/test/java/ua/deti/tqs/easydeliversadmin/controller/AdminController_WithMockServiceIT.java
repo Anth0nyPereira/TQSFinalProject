edd146b9a1bd68f63d9f7de6eba7c39d3b5c6f50
Anth0nyPereira <anthonypereira@ua.pt>
2021-06-15 02:40:27 +0100

    Removed unnecessary imports

--- EasyDelivers/Platform/EasyDeliversAdmin/src/test/java/ua/deti/tqs/easydeliversadmin/controller/AdminController_WithMockServiceIT.java
+++ EasyDelivers/Platform/EasyDeliversAdmin/src/test/java/ua/deti/tqs/easydeliversadmin/controller/AdminController_WithMockServiceIT.java
@@ -1,30 +1,18 @@
 
 package ua.deti.tqs.easydeliversadmin.controller;
 
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ua.deti.tqs.easydeliversadmin.entities.Admin;
 import ua.deti.tqs.easydeliversadmin.service.EasyDeliversService;
-import ua.deti.tqs.easydeliversadmin.utils.JsonUtil;
 import ua.deti.tqs.easydeliversadmin.utils.PasswordEncryption;
-
-import java.util.Arrays;
-
-import static org.hamcrest.CoreMatchers.is;
-
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(EasyDeliversController.class)
