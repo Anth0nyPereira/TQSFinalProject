80bedead306ed9233f64cb3d0954d8b0048296a4
Anth0nyPereira <anthonypereira@ua.pt>
2021-06-21 20:51:07 +0100

    Merge with develop

--- EasyDelivers/Platform/EasyDeliversAdmin/src/test/java/ua/deti/tqs/easydeliversadmin/utils/GeocoderUnitTest.java
+++ EasyDelivers/Platform/EasyDeliversAdmin/src/test/java/ua/deti/tqs/easydeliversadmin/utils/GeocoderUnitTest.java
@@ -3,7 +3,6 @@ package ua.deti.tqs.easydeliversadmin.utils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ua.deti.tqs.easydeliversadmin.component.Geocoder;
