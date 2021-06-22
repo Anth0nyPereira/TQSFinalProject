f366a0d5e001864dfb148082dbd154048d288686
Hugo Ferreira <hugodinis@ua.pt>
2021-06-22 04:18:22 +0100

    fix docker

--- EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/component/Geocoder.java
+++ EasyDelivers/Platform/EasyDeliversAdmin/src/main/java/ua/deti/tqs/easydeliversadmin/component/Geocoder.java
@@ -1,8 +1,8 @@
 package ua.deti.tqs.easydeliversadmin.component;
 
-import org.springframework.boot.configurationprocessor.json.JSONArray;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
